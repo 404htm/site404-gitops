0c48e1475598ab22916213b26c5feeb8352a82dc
Kelly Robins <Kelly@404htm.com>
2022-08-07 02:39:45 -0400

    Modify web buckets

--- environments/dev/site-cloud-storage.tf
+++ environments/dev/site-cloud-storage.tf
@@ -1,7 +1,7 @@
-resource "google_storage_bucket" "web-storage-app" {
-  force_destroy               = false
+resource "google_storage_bucket" "web-application" {
+  force_destroy               = true
   location                    = "US"
-  name                        = "${var.environment}-web-storage-app"
+  name                        = "${var.project_id}_${var.environment}-web-application"
   project                     = var.project_id
   storage_class               = "STANDARD"
   uniform_bucket_level_access = true
@@ -10,4 +10,31 @@ resource "google_storage_bucket" "web-storage-app" {
     main_page_suffix = "index.html"
     not_found_page   = "404.html"
   }
+}
+
+resource "google_storage_bucket_access_control" "web-application-access" {
+  bucket = google_storage_bucket.web-application.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+
+resource "google_storage_bucket" "web-posts" {
+  force_destroy               = true
+  location                    = "US"
+  name                        = "${var.project_id}_${var.environment}-web-posts"
+  project                     = var.project_id
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+resource "google_storage_bucket_access_control" "web-posts-access" {
+  bucket = google_storage_bucket.web-posts.name
+  role   = "READER"
+  entity = "allUsers"
 }
