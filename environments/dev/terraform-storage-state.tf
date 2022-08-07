54b1e55a1b5fcf4349f5dc44247cafd5a95aa6ef
Kelly Robins <Kelly@404htm.com>
2022-08-07 00:02:33 -0400

    Delete random terraform Files - What's the worst that can happen?

--- environments/dev/terraform-storage-state.tf
+++
@@ -1,9 +0,0 @@
-resource "google_storage_bucket" "tf-storage-state" {
-  name          = "tf-storage-state"
-  force_destroy = false
-  location      = "US"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-}
